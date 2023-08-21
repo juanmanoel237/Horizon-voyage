7a79e938cd5aea49da93e27220dbf05b3728d272
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2023-08-21 10:27:03 +0200

    Rendre la navbar dynamique(première partie)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,21 @@ import "./NavbarStyles.css";
 import { MenuItems } from "./MenuItems";
 
 class Navbar extends Component {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
   render() {
     return (
       <nav className="NavbarItems">
         <h1 className="navbar-logo">Trippy</h1>
+
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
+
         <ul className="nav-menu">
           {MenuItems.map((item, index) => {
             return (
