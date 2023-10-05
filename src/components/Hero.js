0a85e5928e5a7c621a113047718b8bd7902ae845
juanmanoel237 <ndeguemanoeljuan@gmail.com>
2023-10-05 13:20:09 +0200

    Création des props pour la section hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,21 @@
-import React from "react";
 import "./HeroStyles.css";
 
-const Hero = (props) => {
+function Hero(props) {
   return (
     <>
       <div className={props.cName}>
-        <img src={props.heroImg} alt="HeroImage" />
+        <img src={props.heroImg} alt="HeroImg" />
 
         <div className="hero-text">
           <h1>{props.title}</h1>
           <p>{props.text}</p>
-          <a href="/">Plan de voyage</a>
+          <a href={props.url} className={props.btnClass}>
+            {props.buttonText}
+          </a>
         </div>
       </div>
     </>
   );
-};
+}
 
 export default Hero;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,7 +4,12 @@ function Home() {
   return (
     <>
       <Navbar />
-      <Hero />
+      <Hero
+        cName="hero"
+        heroImg="https://images.unsplash.com/photo-1502307941198-c79259da770a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+        title="Votre voyage, votre histoire"
+        text="Choisissez votre destination favorite"
+      />
     </>
   );
 }
